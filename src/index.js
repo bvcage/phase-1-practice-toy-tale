68f1018720e5bed34375a7c393b325ba6487929a
Bailey Cage <bailey.cage@gmail.com>
2022-08-16 10:45:09 -0400

    fetch toys and create cards

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
+
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
     addToy = !addToy;
@@ -12,4 +14,19 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  // get toy inventory
+  fetch('http://localhost:3000/toys')
+  .then((response) => response.json())
+  .then((data) => logToys(data))
+  .catch((error) => console.log(error));
+
+  function logToys (inventory) {
+    const toyCollection = document.querySelector('div#toy-collection');
+    Object.values(inventory).forEach((toy) => {
+      const newCard = document.createElement('div');
+      newCard.classList.add('card');
+      toyCollection.appendChild(newCard);
+    });
+  }
 });
